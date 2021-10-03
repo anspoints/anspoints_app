85876092d92ef8fa62e4f0870788a6fe88aa2365
Aditi Panchal <aditipanchal121@gmail.com>
2021-10-02 19:46:40 -0500

    Initialized points

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :points
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root :to => 'index#index'
+  get '/points', to: 'points#index'
 end
