cef41ad433e45b9c11e0cae84844c01294e2e6ca
Aditi Panchal <aditipanchal121@gmail.com>
2021-10-04 18:35:53 -0500

    finished admin points dashboard

--- app/controllers/points_controller.rb
+++ app/controllers/points_controller.rb
@@ -2,5 +2,6 @@ class PointsController < ActionController::Base
     def index
       # put any code here that you need 
       # (although for a static view you probably won't have any)
+      @user_points_counts = EventsUsers.select('"users"."email" AS email, count(*) AS count').joins('INNER JOIN "users" ON "events_users"."user_id" = "users"."id"').group('"users"."email"')
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :events, :contacts, :points
   get '/users/search'
   get '/users/show'
+  get '/points', to: 'points#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'events#index'
-  get '/points', to: 'points#index'
 end
