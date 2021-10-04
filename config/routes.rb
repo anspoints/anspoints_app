4427bd1dba87b3db543c72d37d777010bed115f4
Aditi Panchal <aditipanchal121@gmail.com>
2021-10-04 18:59:23 -0500

    fixed merge conflicts

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  resources :events, :contacts
+  resources :events, :contacts, :points
   get '/users/search'
   get '/users/show'
+  get '/points', to: 'points#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'events#index'
 end
