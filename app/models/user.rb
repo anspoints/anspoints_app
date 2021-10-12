7106da9ed7545057f8e121d8daba9ab79a2654c4
Eta <90056552+SyraAM@users.noreply.github.com>
2021-10-12 12:31:09 -0500

    Made admin default to false

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  attribute :isAdmin, :boolean, default: false
   validates :email, presence: true
   validates :isAdmin, inclusion: { in: [true, false] }
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
