590043c4aca47e3990b51f92298efa35a23f60ed
Gregory Petri <gcpetri@outlook.com>
2021-10-21 15:50:30 -0500

    style updates

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,15 +16,29 @@ ActiveStorage.start()
 
 //= require_tree.
 
+const themeDarkGrey = "#474B4F";
+const themeDarkBlue = "#3A546C";
+const themeBackgroundColor = "#F0F0F0";
+const whiteColor = "#FFF";
+
 function requestEventPassword(eventId, eventName) {
     console.log(eventId);
     console.log(eventName);
+}
+
+function handleNavClick(event) {
+    console.log(this);
+    console.log(this.style.color);
+    console.log(this.style.background);
+    this.style.color = this.style.color === themeDarkGrey ? whiteColor : themeDarkGrey;
+    this.style.backgroundColor = this.style.backgroundColor === themeBackgroundColor ? themeDarkBlue : themeBackgroundColor;
 }
   
 window.addEventListener("load", () => {
     const links = document.querySelectorAll(
         "a[data-event-id]"
     );
+    console.log(links); 
     links.forEach((element) => {
         element.addEventListener("click", (event) => {
             event.preventDefault();
@@ -32,6 +46,21 @@ window.addEventListener("load", () => {
     
             const {eventId, eventName} = element.dataset;
             requestEventPassword(eventId, eventName);
+        });
+    });
+    const navLinks = document.querySelectorAll(
+        "a.nav-link"
+    );
+    navLinks.forEach((element) => {
+        element.addEventListener("click", (event) => {
+            console.log(element.style.color)
+            console.log(element.style.backgroundColor);
+            navLinks.forEach(ele => {
+                ele.style.color = whiteColor;
+                ele.style.backgroundColor = themeBackgroundColor;
+            });
+            element.style.color = themeDarkGrey;
+            element.style.backgroundColor = themeBackgroundColor;
         });
     });
 });
