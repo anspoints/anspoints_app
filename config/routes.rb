2c056f0ee11afd1faa4cca0388c5e0b85b77bb7c
Ben Paul <benjabot97@gmail.com>
2021-10-24 22:26:17 -0500

    Done with Devise Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admins
+  devise_for :admins, controllers: { omniauth_callbacks: 'admins/omniauth_callbacks' }
+  devise_scope :admin do
+    get 'admins/sign_in', to: 'admins/sessions#new', as: :new_admin_session
+    get 'admins/sign_out', to: 'admins/sessions#destroy', as: :destroy_admin_session
+  end
   mount RailsAdmin::Engine => '/admin', :as => 'rails_admin'
   resources :contacts, :points
   resources :events do
