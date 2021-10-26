e56b986118fec1fe8bea8d908deaf246b5ea6678
Ben Paul <benjabot97@gmail.com>
2021-10-25 20:54:55 -0500

    WORKING

--- app/controllers/rails_admin/main_controller.rb
+++
@@ -1,9 +0,0 @@
-module RailsAdmin
-    class MainController < RailsAdmin::ApplicationController
-        before_action :authenticate_user!
-
-        def check_admin_authorization!
-            render file: 'public/401.html', layout: false, status: :unauthorized unless current_user.isAdmin == true       
-        end
-    end
-end
--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -11,9 +11,14 @@ RailsAdmin.config do |config|
 
   config.authenticate_with do
     warden.authenticate! scope: :user
-    check_admin_authorization!
   end
   config.current_user_method(&:current_user)
+
+  RailsAdmin.config do |config|
+    config.authorize_with do
+      redirect_to main_app.root_path unless current_user.isAdmin
+    end
+  end
 
   ## == CancanCan ==
   # config.authorize_with :cancancan
