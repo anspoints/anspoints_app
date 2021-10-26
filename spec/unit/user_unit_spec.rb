893ecdb2bee76ef29d463ab1b96e155a1ef641cd
Eta <90056552+SyraAM@users.noreply.github.com>
2021-10-25 21:49:12 -0500

    Updated rspec tests

--- spec/unit/user_unit_spec.rb
+++ spec/unit/user_unit_spec.rb
@@ -5,7 +5,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   subject do
-    described_class.new(email: 'ryan@tamu.edu')
+    described_class.new(email: 'ryan@tamu.edu', first_name: "Ryan", last_name: "Surname")
   end
 
   it 'is valid with valid attributes' do
@@ -13,7 +13,17 @@ RSpec.describe User, type: :model do
   end
 
   it 'is not valid, missing required values' do
+    old_email = subject.email
     subject.email = nil
+    expect(subject).not_to be_valid
+    subject.email = old_email
+
+    old_first_name = subject.first_name
+    subject.first_name = nil
+    expect(subject).not_to be_valid
+    subject.first_name = old_first_name
+
+    subject.last_name = nil
     expect(subject).not_to be_valid
   end
 end
