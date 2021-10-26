5d4b4918067020a686eeaa5415f9854de7f428e8
Ben Paul <benjabot97@gmail.com>
2021-10-25 21:16:39 -0500

    Open to any gmail account

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:google_oauth2]
 
   def self.from_google(options)
-    return nil unless options[:email] =~ /@tamu.edu\z/
+    # return nil unless options[:email] =~ /@tamu.edu\z/
     create_with(uid: options[:uid]).find_or_create_by!(email: options[:email])
   end
 end
