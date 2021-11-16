1bfc57f05f7f24f9817e7d27eac35b5f05dceeb0
Ben Paul <benjabot97@gmail.com>
2021-11-15 19:00:22 -0600

    Fixed Having Too Many Users

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -20,6 +20,7 @@ RailsAdmin.config do |config|
         unless current_user.isAdmin
           flash[:error] = "#{current_user.email} is not an admin! Please try again with a different user
             or contact an administrator."
+          User.find(current_user.id).destroy unless current_user.last_name != 'lastAdmin'
           redirect_to '/users/sessions/failed'
         end
       end
